8bb45143cac5e5ced9db8fad75d4d544e3f7cfea
Dansky <lykimphong2001@example.com>
2022-11-29 13:18:47 +0700

    tạo trang projects

---
+++ header.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
+    <link rel="stylesheet" href="/admin/dataTables.bootstrap.css">
+    <link rel="stylesheet" href="/admin/dt-sidebar.css">
+    <link rel="stylesheet" href="/admin/dt-gradients.css">
+    <link rel="stylesheet" href="/admin/dt-theme.css">
+    <link rel="stylesheet" href="/admin/dt-styles.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+    <script src="/js/jquery-3.2.1.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/jquery.dataTables.js"></script>
+    <script src="/js/dataTables.bootstrap.js"></script>
+</head>
+
+<body>
+  
+    <div id="page-content-wrapper">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          </div>
+        </div>
+      </nav>
+      <div class="container-fluid">
+        <div class="row">
+         
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="/js/admin/simple-sidebar.js"></script>
+  <script src="/js/admin/plugins.js"></script>
+
+</body>
+
+</html>
