ced269891c200e4d90a067c96188fd4bea9d9eeb
Dansky <lykimphong2001@example.com>
2022-12-05 16:41:45 +0700

    commit

--- login/login.php
+++ login/login.php
@@ -1,4 +1,17 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Company-HR</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+</html>
 <body>
   <div class="bg-white mx-5">
       <div class="row">
@@ -34,7 +47,8 @@
           </div>
         </div>
         <div class="col-sm-6 d-flex flex-wrap align-items-center">
-          <img src="./login/login-img.png" alt="Login image" class="img-fluid mx-auto d-block">
+          <img src="login-img.png" alt="Login image"
+            class="img-fluid mx-auto d-block">
         </div>
       </div>
     </div>
