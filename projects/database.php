6b55214160ebd2ecc177e4e6218dd249fbd9cb16
Vu Tran Minh <minhvu120201dn.nkhv@gmail.com>
2022-12-05 16:30:25 +0700

    merged with main

--- projects/database.php
+++ projects/database.php
@@ -1,6 +1,10 @@
 <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = 'company-hr';
 /////   CREATE DATABASE     /////
-$conn = connect_to_database();
+$conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 }
