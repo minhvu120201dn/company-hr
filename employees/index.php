579d0db3bf558989093a2f5d84ffa4ef5ea514ec
Vu Tran Minh <minhvu120201dn.nkhv@gmail.com>
2022-12-07 15:15:23 +0700

    employee page without sql scripts

--- employees/index.php
+++ employees/index.php
@@ -1,13 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <!-- <title>Company hr</title> -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <?php
-        include "signup.php";
-        ?>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css">
+
+    <script src="https://kit.fontawesome.com/bd5d5fa01b.js" crossorigin="anonymous"></script>
+
+    
+    <link rel="stylesheet" href="./public/css/dataTables.bootstrap.css">
+    <link rel="stylesheet" href="./public/css/dt-sidebar.css">
+    <link rel="stylesheet" href="./public/css/dt-gradients.css">
+    <link rel="stylesheet" href="./public/css/dt-theme.css">
+    <link rel="stylesheet" href="./public/css/dt-styles.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script data-require="jquery@*" data-semver="3.0.0" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.js"></script>
+    <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables_themeroller.css">
+    <script src="./public/js/bootstrap.min1.js"></script>
+    <script src="./public/js/bootstrap.min.js"></script>
+    <script src="./public/js/dataTables.bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="./public/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="./public/js/jquery.dataTables.js"></script>
+
+    <script src="../script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <title>
+    </title>
+</head>
+
+<body>
+    <?php
+    session_start();
+    include "../header.php";
+    ?>
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <?php
+            include "employees.php";
+            ?>
+        </div>
+    </div>
+</body>
+
 </html>
--- projects/index.php
+++ projects/index.php
@@ -32,6 +32,7 @@
 </head>
 <body>
     <?php
+    session_start();
     include "../header.php";
     ?>
     <div id="page-content-wrapper">
